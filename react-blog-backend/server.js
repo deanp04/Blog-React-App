5c35aa35c1929120796246e1445f546288a8df08
Dean Patterson <deanp04@gmail.com>
2023-09-22 22:59:25 -0400

    completed server

---
+++ react-blog-backend/server.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+require('dotenv').config()
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log('Connected to DB!'))
+    .catch(error => console.log(error.message))
+
+const postSchema = new mongoose.Schema({
+    title: String,
+    author: String,
+    image: String,
+    content: String,
+})
+
+const Post = mongoose.model('Post', postSchema)
+
+// CONTROLLERS
+// get all posts
+app.get('/posts', async (req,res) => {
+    const posts = await Post.find()
+    res.send(posts)
+})
+// get one post
+app.get('/posts/:id', async (req,res) => {
+    const posts = await Post.findById(req.params.id)
+    res.send(post)
+})
+// create new post
+app.post('/posts', async (req,res) => {
+    const newPost = new Post(req.body)
+    const savedPost = await newPost.save()
+    res.send(savePost)
+})
+// delete post
+app.delete('/posts/:id'), async (req,res) => {
+    await Post.findByIdAndDelete(req.params.id)
+    res.status(200).send('Post deleted')
+}
+
+app.listen(5500, () => console.log('Server started on port 5500'))
