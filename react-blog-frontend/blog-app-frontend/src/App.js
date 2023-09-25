c4b7e75e9463d69324dbd2afffc41c420d676265
Dean Patterson <deanp04@gmail.com>
2023-09-25 10:41:30 -0400

    set up routes

--- react-blog-frontend/blog-app-frontend/src/App.js
+++ react-blog-frontend/blog-app-frontend/src/App.js
@@ -1,8 +1,20 @@
-
+import React from "react"
+import {Route, BrowserRouter as Router, Routes} from "react-router-dom"
+import HomePage from './pages/HomePage'
+import PostPage from './pages/PostPage'
+import NewPostPage from './pages/NewPostPage'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
-    
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<HomePage/>} />
+        <Route path='/posts/new' element={<NewPostPage/>} />
+        <Route path='/posts/:id' element={<PostPage/>} />
+      </Routes>
+    </Router>
   );
 }
 
